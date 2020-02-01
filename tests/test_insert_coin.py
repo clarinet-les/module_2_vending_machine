dd561b0797ffaa97234caf8fcb05271e8fb6ac62
Lillian Stubbs <l.stubbs@utoronto.ca>
2020-02-01 14:26:40 -0500

    Added tests for step 5

--- tests/test_insert_coin.py
+++ tests/test_insert_coin.py
@@ -19,3 +19,31 @@ def test_fifty_cents():
     inserted_coins = []
     with pytest.raises(ValueError):
         vending_machine.insert_coin(50, inserted_coins)
+
+def test_ten_cents():
+    """
+    Asserts that given a 10 cent coin, the vending machine receives the coin.
+    """
+    inserted_coins = []
+    assert vending_machine.insert_coin(10, inserted_coins) == [10]
+
+def test_twenty_five_cents():
+    """
+    Asserts that given a 25 cent coin, the vending machine receives the coin.
+    """
+    inserted_coins = []
+    assert vending_machine.insert_coin(25, inserted_coins) == [25]
+
+def test_one_dollar():
+    """
+    Asserts that given a 1 dollar coin, the vending machine receives the coin.
+    """
+    inserted_coins = []
+    assert vending_machine.insert_coin(100, inserted_coins) == [100]
+
+def test_two_dollars():
+    """
+    Asserts that given a 2 dollar coin, the vending machine receives the coin.
+    """
+    inserted_coins = []
+    assert vending_machine.insert_coin(200, inserted_coins) == [200]
