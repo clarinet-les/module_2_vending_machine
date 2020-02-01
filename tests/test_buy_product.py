4bcdce189b14d98e52b9b5280244a20af5594d9e
Lillian Stubbs <l.stubbs@utoronto.ca>
2020-02-01 15:41:28 -0500

    Added  test for part of step 5

--- tests/test_buy_product.py
+++ tests/test_buy_product.py
@@ -28,3 +28,11 @@ def test_buy_candy():
     Asserts that a drink can be purchased
     """
     assert vending_machine.buy_product("candy", 315) == 0
+
+def test_insufficient_funds():
+    """
+    Asserts when the customer has not entered enough funds, the item cannot be purchased.
+    In this test, teh product costs 275, the customer has 274.
+    """
+    with pytest.raises(vending_machine.InsufficientFunds):
+        assert vending_machine.buy_product("drink", 274)
--- vending_machine.py
+++ vending_machine.py
@@ -14,11 +14,18 @@ def insert_coin(coin, inserted_coins):
     return inserted_coins
 
 def buy_product(product, balance):
+    """
+    Function to purchase drinks, candy and chips.
+    """
     valid_products = ["drink", "candy", "chips"]
     if product not in valid_products:
         raise ValueError
 
     product_cost = {"drink":275, "chips":225, "candy":315}
+    price = product_cost[product]
+    if price > balance:
+        raise InsufficientFunds
+
     remaining_balance = balance - product_cost[product]
     return remaining_balance
 
@@ -27,4 +34,3 @@ class InsufficientFunds(Exception):
     Exception used to indicate that there isn't
     enough money to make a purchase.
     """
-    pass
